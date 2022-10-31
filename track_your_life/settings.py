088ab8057567732552f81fc3b83a6d5c802d42b5
RomanC451 <catalin.roman451@gmail.com>
2022-10-31 18:23:07 +0200

    Heroku deploiment requirements 2

--- track_your_life/settings.py
+++ track_your_life/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,8 +119,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = "/frontend/static"
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
 
 STATIC_URL = "static/"
 
